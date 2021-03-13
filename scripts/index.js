87a2a8152d659f1cd759b92e257cd9bdbb84dd2f
ALIAS-001 <caleb.eleven@gmail.com>
2021-03-13 13:13:40 -0800

    Function clean up

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,12 @@
-const market = require("markets.js")
+const market = require("./markets.js")
+const URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=community-food-markets-and-farmers-markets&rows=100";
 
+
+function handle_db(results){
+    console.log(results);
+}
+
+market.db_update(URL, function(results){
+    console.log('recieved');
+    handle_db(results);
+})
--- scripts/markets.js
+++ scripts/markets.js
@@ -1,21 +1,11 @@
 //uses records API to GET up-to-date data from site
 const axios = require('axios')
-const URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=community-food-markets-and-farmers-markets&rows=100";
 
 function db_update(URL,callback){
     axios.get(URL)
         .then((res) => {
-           //output in console all available data 1 by 1
             callback(res.data.records);
         })
     .catch((err) => console.log(err));
 };
-
-function handle_db(results){
-    console.log(results);
-}
-
-db_update(URL, function(results){
-    console.log('recieved');
-    handle_db(results);
-})
+module.exports = {db_update}
