de4352cfed7c11a198ab78198669437dd324c811
caliLai <calilai2018@gmail.com>
2021-03-13 13:09:59 -0800

    minor changes

--- scripts/gardens.js
+++ scripts/gardens.js
@@ -5,18 +5,24 @@
 ****/
 
 //modules
-const axios = require("axios");
+// const axios = require("axios");
 // const utilities = require("./utilities");
+const fetch = require(".node-fetch");
 
 const CITY_URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=community-gardens-and-food-trees&q=&rows=5&sort=mapid&facet=mapid&facet=name&facet=merged_address&facet=notes&facet=food_tree_varieties&facet=public_e_mail&facet=website&facet=geom";
+//
+// let data = (url) => {
+// 	axios.get(url)
+// 	.then((res) => console.log(res.data.records))
+// 	.catch((err) => console.log(err))
+// }
+// let a = data(CITY_URL);
+// console.log(a);
 
-let data = (url) => {
-	axios.get(url)
-	.then((res) => console.log(res.data.records))
-	.catch((err) => console.log(err))
-}
+const a = await fetch(CITY_URL);
+// console.log(a);
 
-data(CITY_URL).filter(thing => thing.jurisdiction);
+// a.filter(thing => thing.record_timestamp);
 
 // let fil = utilities.filterer(a, "jurisdiction", "Park Board");
 // console.log(fil);
