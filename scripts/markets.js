b21b9fcfdfeed10debb24863c8e9ff6725512e31
ALIAS-001 <caleb.eleven@gmail.com>
2021-03-13 12:51:47 -0800

    not sure wether to seperate into different functions

--- scripts/markets.js
+++ scripts/markets.js
@@ -1,13 +1,12 @@
 //uses records API to GET up-to-date data from site
 const axios = require('axios')
+const URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=community-food-markets-and-farmers-markets&rows=100";
 
 function db_update(URL){
     axios.get(URL)
         .then((res) => {
            //output in console all available data 1 by 1
-            for(x in res.data.records){
-                return res.data;
-            }
+            return res.data.records
         })
     .catch((err) => console.log(err));
 };
