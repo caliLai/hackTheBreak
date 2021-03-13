a7416b543751e361b2004ec28d8c82cd9d61ed87
caliLai <calilai2018@gmail.com>
2021-03-13 12:06:56 -0800

    organisation

---
+++ scripts/gardens.js
@@ -0,0 +1,18 @@
+/****
+	BCIT HACK THE BREAK 2021
+
+	PAGE FOR VANCOUVER'S COMMUNITY GARDENS AND FOOD TREES
+****/
+
+//modules
+const axios = require("axios");
+
+const CITY_URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=community-gardens-and-food-trees&q=&rows=5&sort=mapid&facet=mapid&facet=name&facet=merged_address&facet=notes&facet=food_tree_varieties&facet=public_e_mail&facet=website&facet=geom";
+
+let data = (url) => {
+	axios.get(url)
+	.then((res) => console.log(res.data.records))
+	.catch((err) => console.log(err))
+}
+
+data(CITY_URL);
