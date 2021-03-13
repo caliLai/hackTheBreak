d98c39c0a918264c5bf7c8db859a9560a1b97f6b
ALIAS-001 <caleb.eleven@gmail.com>
2021-03-13 13:07:42 -0800

    api function works now

--- scripts/markets.js
+++ scripts/markets.js
@@ -2,12 +2,20 @@
 const axios = require('axios')
 const URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=community-food-markets-and-farmers-markets&rows=100";
 
-function db_update(URL){
+function db_update(URL,callback){
     axios.get(URL)
         .then((res) => {
            //output in console all available data 1 by 1
-            return res.data.records
+            callback(res.data.records);
         })
     .catch((err) => console.log(err));
 };
-module.exports = {db_update};
+
+function handle_db(results){
+    console.log(results);
+}
+
+db_update(URL, function(results){
+    console.log('recieved');
+    handle_db(results);
+})
