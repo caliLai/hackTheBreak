b4ab48644a5a7c45a462d61d86e0360e2d357f71
Kev Truong <kevtrng00@gmail.com>
2021-03-13 16:38:09 -0800

    index.html and css added

--- scripts/markets.js
+++ scripts/markets.js
@@ -1,14 +1,11 @@
 //uses records API to GET up-to-date data from site
 const axios = require('axios')
 
-function db_update(URL){
+function db_update(URL,callback){
     axios.get(URL)
         .then((res) => {
-           //output in console all available data 1 by 1
-            for(x in res.data.records){
-                return res.data;
-            }
+            callback(res.data.records);
         })
     .catch((err) => console.log(err));
 };
-module.exports = {db_update};
+module.exports = {db_update}
