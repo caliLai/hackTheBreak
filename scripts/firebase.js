a619a28c9552ef8243cdfb747ec8e62b9641efdf
caliLai <calilai2018@gmail.com>
2021-03-13 16:53:38 -0800

    added firebase

---
+++ scripts/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import dotenv from "dotenv";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: "local-scout.firebaseapp.com",
+  projectId: "local-scout",
+  storageBucket: "local-scout.appspot.com",
+  messagingSenderId: "81379274507",
+  appId: "1:81379274507:web:f9c15d01e3642486837c0e",
+  measurementId: "G-8JZ1YVQTNR"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(app);
